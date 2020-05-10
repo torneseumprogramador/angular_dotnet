7fb5fa2e2dc110ba099283dc72ac50175963c7e7
Danilo <didox_59@yahoo.com.br>
2020-05-10 13:22:02 -0300

    nova aula angular

--- src/app/clientes/cliente.ts
+++ src/app/clientes/cliente.ts
@@ -1,4 +1,5 @@
 export class Cliente {
+  id:number
   nome:string
   telefone:string
   endereco:string
--- src/app/clientes/clientes.component.ts
+++ src/app/clientes/clientes.component.ts
@@ -29,13 +29,41 @@ export class ClientesComponent implements OnInit {
   }
 
   Salvar(){
-    this.http.post(this.REST_API_SERVER, this.cliente).subscribe((clienteRest: Cliente)=>{
-      this.cliente = new Cliente()
-      this.mensagem = "Usuario cadastrado com sucesso!"
-      setTimeout(() => {
-        this.mensagem = undefined
-      }, 2000)
-      this.carregarLista()
-    })
+    if(!this.cliente.id || this.cliente.id === 0){
+      this.http.post(this.REST_API_SERVER, this.cliente).subscribe((clienteRest: Cliente)=>{
+        this.cliente = new Cliente()
+        this.mensagem = "Usuario cadastrado com sucesso!"
+        setTimeout(() => {
+          this.mensagem = undefined
+        }, 2000)
+        this.carregarLista()
+      }) 
+    }
+    else{
+      this.http.put(`${this.REST_API_SERVER}/${this.cliente.id}`, this.cliente).subscribe((clienteRest: Cliente)=>{
+        this.cliente = new Cliente()
+        this.mensagem = "Usuario atualizado com sucesso!"
+        setTimeout(() => {
+          this.mensagem = undefined
+        }, 2000)
+        this.carregarLista()
+      }) 
+    }
+  }
+
+  Alterar(_cliente: Cliente){
+    this.cliente = _cliente
+  }
+
+  Excluir(cliente: Cliente){
+    if(confirm("Confirma ?")){
+      this.http.delete(`${this.REST_API_SERVER}/${cliente.id}`).subscribe(()=>{
+        this.mensagem = "Usuario excluido!"
+        setTimeout(() => {
+          this.mensagem = undefined
+        }, 2000)
+        this.carregarLista()
+      })
+    }
   }
 }
